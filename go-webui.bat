f02b300c182909bc9d2ada7c7d8dd8b68b442aaf
Fallflower <xy673636000@gmail.com>
2024-07-09 22:12:21 +0800

    add check cuda
    make sure that:
        'pip' is in SYSTEM PATH!
        NVIDIA driver support cuda12.1

--- go-webui.bat
+++ go-webui.bat
@@ -1,6 +1,24 @@
 @echo off
 call D:\ProgramData\miniconda3\Scripts\activate.bat gswebui
 
+set temp_file=cuda_check.txt
+
+python -c "import torch;print(torch.cuda.is_available())" > %temp_file%
+
+set /p cuda_available=<%temp_file%
+
+del %temp_file%
+
+if "%cuda_available%"=="True" (
+    echo CUDA is available.
+    REM 在此处添加CUDA可用时要执行的命令
+) else (
+    echo CUDA is not available.
+    REM 在此处添加CUDA不可用时要执行的命令
+    pip3 install torch torchaudio --index-url https://download.pytorch.org/whl/cu121 
+)
+
+
 python.exe webui.py
 pause
 exit /b
